b13664a9991e420cccbad93d8b5ecc716876ddee
kvcnow <kvcnow@gmail.com>
2021-09-13 21:18:36 +0800

    fix: updated media helper functions

--- pkg/protoc-gen-go/chameleon/api/media/media.go
+++ pkg/protoc-gen-go/chameleon/api/media/media.go
@@ -1,8 +1,24 @@
 package media
 
-import anypb "google.golang.org/protobuf/types/known/anypb"
+import (
+	"strings"
+
+	anypb "google.golang.org/protobuf/types/known/anypb"
+)
 
 func NewImageMedia(id, original, large, medium, small, desc string, isDefault bool) *Media {
+	if strings.HasPrefix(original, "//") {
+		original = "https:" + original
+	}
+	if strings.HasPrefix(large, "//") {
+		large = "https:" + large
+	}
+	if strings.HasPrefix(medium, "//") {
+		medium = "https:" + medium
+	}
+	if strings.HasPrefix(small, "//") {
+		small = "https:" + small
+	}
 	data, _ := anypb.New(&Media_Image{
 		Id:          id,
 		OriginalUrl: original,
@@ -18,6 +34,12 @@ func NewImageMedia(id, original, large, medium, small, desc string, isDefault bo
 }
 
 func NewVideoMedia(id, typ, url string, width, height, duration int, coverUrl string, desc string, isDefault bool) *Media {
+	if strings.HasPrefix(url, "//") {
+		url = "https:" + url
+	}
+	if strings.HasPrefix(coverUrl, "//") {
+		url = "https" + coverUrl
+	}
 	v := Media_Video{
 		Id:          id,
 		OriginalUrl: url,
